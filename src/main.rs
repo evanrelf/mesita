27e2d0cab69e8a85437e6b4e70caafc9f48d5b7b
Evan Relf <evan@evanrelf.com>
2025-04-15 21:11:57 -0700

    Hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use ratatui::{
+    crossterm::{
+        self,
+        event::{self, Event},
+    },
+    text::Text,
+};
+use std::io;
+
+fn main() -> anyhow::Result<()> {
+    let mut terminal = ratatui::init();
+    crossterm::execute!(io::stdout(), event::EnableMouseCapture)?;
+    loop {
+        terminal.draw(|frame| {
+            let text = Text::raw("Hello World!");
+            frame.render_widget(text, frame.area());
+        })?;
+        if matches!(event::read()?, Event::Key(_)) {
+            break;
+        }
+    }
+    ratatui::restore();
+    Ok(())
 }
